f341654ac46194c852fd9a493cc82c8deb372e7d
Nikoloz Razmadze <raloz1@gmail.com>
2020-09-21 19:50:31 +0400

    Add home component content

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/services/users-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+
+  public user: User;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getObservable().subscribe(user => this.user = user);
+  }
+}
